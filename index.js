5e9f4794e3ce5da9eb84acb932f04f744ed93e7d
Isaac Davidson <isaac.davidson@virginaustralia.com>
2024-07-04 09:47:46 +1000

    Added mouseover functionality

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ for (let i = 0; i < grid.y; i++) {
     const colItems = document.createElement("div");
     colItems.classList.add("column");
     rowItems.appendChild(colItems);
+    colItems.addEventListener("mouseover", () => {
+      colItems.setAttribute("style", "background-color: grey");
+    })
   }
 
   container.appendChild(rowItems);
 }
+
