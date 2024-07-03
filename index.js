9f2b4a9dd58942f0d614e3707e17bf715ce0a85a
Isaac Davidson <isaac.davidson@virginaustralia.com>
2024-07-03 17:06:17 +1000

    Square grid working
    Would like to limit the size of the container to not take up the entire
    viewport, but will have to do some more research first.

---
+++ index.js
@@ -0,0 +1,24 @@
+const container = document.querySelector("#container")
+
+const grid = {
+  x: 16,
+  y: 16
+}
+
+// grid.x = 100;
+// grid.y = 100;
+
+const numberItems = (grid.x * grid.y) - 1;
+
+for (let i = 0; i < grid.y; i++) {
+  const rowItems = document.createElement("div");
+  rowItems.classList.add("row");
+
+  for (let i = 0; i < grid.x; i++) {
+    const colItems = document.createElement("div");
+    colItems.classList.add("column");
+    rowItems.appendChild(colItems);
+  }
+
+  container.appendChild(rowItems);
+}
