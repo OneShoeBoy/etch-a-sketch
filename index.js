4ee8010f0170b76b14b693724c10a31433b47914
Isaac Davidson <isaac.davidson@virginaustralia.com>
2024-07-04 11:37:02 +1000

    Added ability to set custom grid size
    Page will render with a 16x16 grid by default. Added a button to allow
    for user specified x and y values, up to a max of 100 in both axes.

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const header = document.querySelector("#header");
+const setGrid = document.createElement("button");
+setGrid.textContent = "Set grid size";
+
+header.appendChild(setGrid);
+
+function generateGrid(grid) {
+  for (let i = 0; i < grid.y; i++) {
+    const rowItems = document.createElement("div");
+    rowItems.classList.add("row");
+
+    for (let i = 0; i < grid.x; i++) {
+      const colItems = document.createElement("div");
+      colItems.classList.add("column");
+      rowItems.appendChild(colItems);
+      colItems.addEventListener("mouseover", () => {
+        colItems.classList.add("mouseOver");
+      })
+    }
+    container.appendChild(rowItems);
+  }
+
+
+}
 
 const grid = {
   x: 16,
   y: 16
 }
 
+generateGrid(grid);
+
+setGrid.addEventListener("click", () => {
+  let gridComplete;
+
+  while (!gridComplete) {
+    let xPrompt = Number(prompt("Specify X dimensions (max 100)"));
+    if (xPrompt > 100) {
+      alert("Number must be 100 or less");
+      continue;
+    }
+
+    let yPrompt = Number(prompt("Specify Y dimensions (max 100)"));
+    if (yPrompt > 100) {
+      alert("Number must be 100 or less");
+      continue;
+    }
+
+    if (xPrompt <= 100 && yPrompt <= 100) {
+      grid.x = xPrompt;
+      grid.y = yPrompt;
+      gridComplete = true;
+      break;
+    }
+
+    grid.x = Number(prompt("Specify X dimensions (max 100)"));
+    grid.y = Number(prompt("Specify Y dimensions (max 100)"));
+
+  }
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    container.removeChild(row)
+  })
+  generateGrid(grid);
+
+})
+
+
+
 // grid.x = 100;
 // grid.y = 100;
 
-const numberItems = (grid.x * grid.y) - 1;
-
-for (let i = 0; i < grid.y; i++) {
-  const rowItems = document.createElement("div");
-  rowItems.classList.add("row");
-
-  for (let i = 0; i < grid.x; i++) {
-    const colItems = document.createElement("div");
-    colItems.classList.add("column");
-    rowItems.appendChild(colItems);
-    colItems.addEventListener("mouseover", () => {
-      colItems.setAttribute("style", "background-color: grey");
-    })
-  }
-
-  container.appendChild(rowItems);
-}
 
